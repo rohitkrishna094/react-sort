41b90f52d87ff888d6c664f479820551ef7797cd
rohitkrishna094 <rohitkrishna094@gmail.com>
2020-05-12 19:24:33 -0400

    removed old project and updated todo.txt

--- src/store/reducers/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import chartReducer from './chartReducer';
-
-const rootReducer = combineReducers({
-  chart: chartReducer
-});
-
-export default rootReducer;
