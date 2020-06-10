2516995ed0a3fdf2066f13ff684ceb73cbefbb98
rohitkrishna094 <18495886+rohitkrishna094@users.noreply.github.com>
2020-06-10 20:55:39 +0000

    Deploying to gh-pages from  @ b254248ca00787f63c8ceef187009c2e6c2b5ffa 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7c8e4703951ff0287802b7241357f4e8.js"
+  "/react-sort/precache-manifest.6314a732e07bdbba763d33d931e45777.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-sort/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
